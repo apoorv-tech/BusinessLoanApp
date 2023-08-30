62c311b4b43ce675a46820252888f228e95d3c88
apoorv-tech <apoorvpriyadarsh1729@gmail.com>
2023-08-30 14:08:19 +0530

    Create Start route

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const app = express()
+const uuid = require('uuid');
 const port = 4000
 
+const unique_applications = {}
 
-app.get('/',async(req,res)=>{
-    console.log("Welcome to the App!!");
+app.get('/api/start',async(req,res)=>{
+    try {
+        let uniqueID = uuid.v4();
+        while(unique_applications[uniqueID]){
+            uniqueID = uuid.v4()
+        }
+        unique_applications[uniqueID] = true
+        console.log(unique_applications);
+        console.log(`generated unique id: ${uniqueID}`)
+        res.status(200).send(uniqueID)
+    } catch (error) {
+        console.log("error occurred while initializing loan application",error);
+        res.status(500).send("Internal Server Error")
+    }
 })
 
 app.listen(port,()=>{
