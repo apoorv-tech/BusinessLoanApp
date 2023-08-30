063212ea461c0bfa6401766eebd5370bcee7b4c1
apoorv-tech <apoorvpriyadarsh1729@gmail.com>
2023-08-30 13:49:23 +0530

    Add / route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const app = express()
 const port = 4000
 
 
+app.get('/',async(req,res)=>{
+    console.log("Welcome to the App!!");
+})
+
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 })
